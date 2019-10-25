5931083556d81fd3234f3d8630ef453fb8775d8d
Matt Hopkins <matthops@gmail.com>
2019-10-25 11:49:26 -0700

    testing cronjob

--- server/cronJobs/send.js
+++ server/cronJobs/send.js
@@ -1,35 +1,33 @@
-const Nexmo = require('nexmo');
-const express = require('express');
+// const Nexmo = require('nexmo');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-// module.exports = {
-//   nexmoJob: () => {
-const { NEXMO_API_KEY, NEXMO_API_SECRET } = process.env;
+// const { NEXMO_API_KEY, NEXMO_API_SECRET } = process.env;
 
-const nexmo = new Nexmo({
-  apiKey: NEXMO_API_KEY,
-  apiSecret: NEXMO_API_SECRET
-});
+// const nexmo = new Nexmo({
+//   apiKey: NEXMO_API_KEY,
+//   apiSecret: NEXMO_API_SECRET
+// });
 
-const from = '18704068020';
-const to = '19253033344';
-const text = 'Another New Test Message';
+// const from = '18704068020';
+// const to = '19253033344';
+// const text = 'Another New Test Message';
 
-nexmo.message.sendSms(from, to, text, (err, responseData) => {
-  if (err) {
-    console.log(err);
-  } else {
-    if (responseData.messages[0]['status'] === '0') {
-      console.log('Message sent successfully.');
-    } else {
-      console.log(
-        `Message failed with error: ${responseData.messages[0]['error-text']}`
-      );
-    }
-  }
-});
+// nexmo.message.sendSms(from, to, text, (err, responseData) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     if (responseData.messages[0]['status'] === '0') {
+//       console.log('Message sent successfully.');
+//     } else {
+//       console.log(
+//         `Message failed with error: ${responseData.messages[0]['error-text']}`
+//       );
+//     }
 //   }
-// };
+// });
 
-app.listen(3128);
+// app.listen(3128);
+
+console.log('Is this cron jobw working?');
